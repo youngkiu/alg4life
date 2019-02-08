936fd02625854dcfd8fd708cb5d326cc95479dc2
youngki lyu <youngkiu@gmail.com>
2019-02-08 11:34:44 +0900

    https://www.youtube.com/watch?v=2pdAbrU_7D0&t=28s

--- mergeSort/mergeSort.cpp
+++ mergeSort/mergeSort.cpp
@@ -3,9 +3,85 @@
 
 #include "stdafx.h"
 
+void printArrayForMergeSort(int value[], int start, int middle, int end)
+{
+	int t = 0;
+	printf("sort range: %d~%d, mid-%d, ", start, end, middle);
+	for (t = start; t <= end; t++) {
+		printf("%d ", value[t]);
+	}
+	printf("\n");
+}
+
+void mergeSortInternal(int value[], int buffer[], int start, int middle, int end)
+{
+	int i = 0, j = 0, k = 0, t = 0;
+	i = start;
+	j = middle + 1;
+	k = start;
+
+	while (i <= middle && j <= end) {
+		if (value[i] <= value[j]) {
+			buffer[k] = value[i];
+			i++;
+		}
+		else {
+			buffer[k] = value[j];
+			j++;
+		}
+		k++;
+	}
+
+	if (i > middle) {
+		for (t = j; t <= end; t++, k++) {
+			buffer[k] = value[t];
+		}
+	}
+	else {
+		for (t = i; t <= middle; t++, k++) {
+			buffer[k] = value[t];
+		}
+	}
+
+	for (t = start; t <= end; t++) {
+		value[t] = buffer[t];
+	}
+
+	printArrayForMergeSort(value, start, middle, end);
+}
+
+void mergeSort(int value[], int buffer[], int start, int end)
+{
+	int middle = 0;
+	if (start < end) {
+		middle = (start + end) / 2;
+		mergeSort(value, buffer, start, middle);
+		mergeSort(value, buffer, middle + 1, end);
+		mergeSortInternal(value, buffer, start, middle, end);
+	}
+}
+
+void printArray(int value[], int count)
+{
+	int i = 0;
+	for (i = 0; i < count; i++) {
+		printf("%d ", value[i]);
+	}
+	printf("\n");
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int values[] = { 80, 75, 10, 60, 15, 49, 12, 25 };
+	int pBuffer[8];
+
+	printf("before sort\n");
+	printArray(values, 8);
+
+	mergeSort(values, pBuffer, 0, 7);
+
+	printf("after sort\n");
+	printArray(values, 8);
+
 	return 0;
 }
-
